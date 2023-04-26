063f4559390a93052f770c2e461efde451ad897e
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-04-27 01:31:13 +0530

    add RainResponse

--- src/main/java/com/bloomscorp/raintree/configuration/GsonExclusionStrategy.java
+++ src/main/java/com/bloomscorp/raintree/configuration/GsonExclusionStrategy.java
@@ -2,11 +2,12 @@ package com.bloomscorp.raintree.configuration;
 
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
+import org.jetbrains.annotations.NotNull;
 
 public class GsonExclusionStrategy implements ExclusionStrategy {
 
     @Override
-    public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+    public boolean shouldSkipField(@NotNull FieldAttributes fieldAttributes) {
         return fieldAttributes.getAnnotation(GsonExclude.class) != null;
     }
 
---
+++ src/main/java/com/bloomscorp/raintree/restful/RainResponse.java
@@ -0,0 +1,62 @@
+package com.bloomscorp.raintree.restful;
+
+import com.bloomscorp.hastar.code.ActionCode;
+import com.bloomscorp.raintree.RainTree;
+import com.bloomscorp.raintree.RainTreeResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public abstract class RainResponse<E> {
+
+	private final RainTree rainTree;
+
+	@Contract("_ -> new")
+	public static @NotNull RainTreeResponse prepareActionResponse(int action) {
+
+		boolean success = switch (action) {
+			case 	ActionCode.INSERT_SUCCESS,
+					ActionCode.UPDATE_SUCCESS,
+					ActionCode.DELETE_SUCCESS -> true;
+			default -> false;
+		};
+
+		return new RainTreeResponse(
+			success,
+			ActionCode.message(action)
+		);
+	}
+
+	protected <T> String prepareList(List<? extends T> list, String responseParameter) {
+		HashMap<String, Object> parameters = new HashMap<>();
+
+		parameters.put(
+			responseParameter,
+			list
+		);
+
+		return this.rainTree.renderParameterizedSuccessResponse(parameters);
+	}
+
+	protected <T> String prepareEntity(T object, String responseParameter) {
+
+		HashMap<String, Object> parameters = new HashMap<>();
+
+		parameters.put(
+			responseParameter,
+			object
+		);
+
+		return this.rainTree.renderParameterizedSuccessResponse(parameters);
+	}
+
+	public abstract String buildEntity(E entity);
+	public abstract String buildList(List<E> list);
+	public abstract <P, R> R prepareResponse(P parameter);
+}
