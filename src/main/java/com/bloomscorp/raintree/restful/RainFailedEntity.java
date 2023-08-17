a6b216e8b0881292ebf1f10ef43ff86e06bbdae2
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-08-17 18:31:25 +0530

    update RainFailedEntity

--- src/main/java/com/bloomscorp/raintree/restful/RainFailedEntity.java
+++ src/main/java/com/bloomscorp/raintree/restful/RainFailedEntity.java
@@ -1,5 +1,8 @@
 package com.bloomscorp.raintree.restful;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class RainFailedEntity<E> extends RainEntity<E> {
-	public E entity = null;
+	public E entity;
 }
